6e3d719b6e80a9faec1c56dad6dda6d8ffe55b44
rajkumar3918 <rajkumarpinakana3@gmail.com>
2023-03-13 15:28:42 +0530

    submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homePage';
+import Navbar from './components/navbar';
+import Auth from './components/authantication';
+import Footer from './components/footer';
+
+// import ShoppingCart from './components/cart';
+
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+    <Routes>
+     <Route index element={<HomePage/>}/>
+     <Route path='/myProfile' element={<Auth/>}/>
+
+      </Routes>
+      <Footer/>
+
     </div>
   );
 }
